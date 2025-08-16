21094e4ce813ee8370f4c82d04841947a28a3cbe
Fiantso Harena <fiantsorav@gmail.com>
2025-08-16 14:35:09 +0300

    feat: application main routes

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,8 +1,17 @@
 import { useMemo } from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router';
+
+import { paths } from '@/config/paths';
+
+import { default as AppRoot } from './routes/app/root';
 import LandingRoute from './routes/landing';
 import Register from './routes/auth/register';
-import { paths } from '@/config/paths';
+import Login from './routes/auth/login';
+import Dashboard from './routes/app/dashboard';
+import Expenses from './routes/app/expenses';
+import Incomes from './routes/app/incomes';
+import Categories from './routes/app/categories';
+import Profile from './routes/app/profile';
 
 const createAppRouter = () =>
   createBrowserRouter([
@@ -12,8 +21,38 @@ const createAppRouter = () =>
     },
     {
       path: paths.auth.register.path,
-      element: <Register />
-    }
+      element: <Register />,
+    },
+    {
+      path: paths.auth.login.path,
+      element: <Login />,
+    },
+    {
+      path: paths.app.root.path,
+      element: <AppRoot />,
+      children: [
+        {
+          path: paths.app.dashboard.path,
+          element: <Dashboard />,
+        },
+        {
+          path: paths.app.expenses.path,
+          element: <Expenses />,
+        },
+        {
+          path: paths.app.incomes.path,
+          element: <Incomes />,
+        },
+        {
+          path: paths.app.categories.path,
+          element: <Categories />,
+        },
+        {
+          path: paths.app.profile.path,
+          element: <Profile />,
+        },
+      ],
+    },
   ]);
 
 export const AppRouter = () => {
