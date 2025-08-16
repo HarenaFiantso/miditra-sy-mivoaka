8161ee1f33e37d59da94917d47958284809e8120
Fiantso Harena <fiantsorav@gmail.com>
2025-08-16 13:04:05 +0300

    chore: prettier configuration

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,6 @@
 export default function App() {
   return (
-    <div className="w-full h-screen flex items-center justify-center">
+    <div className="flex h-screen w-full items-center justify-center">
       <h1 className="text-3xl font-bold">Hello world!</h1>
     </div>
   );
