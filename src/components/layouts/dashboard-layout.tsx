4529f2e6dccebd4a5bab9440d2ebbc7d9f34ec89
Fiantso Harena <fiantsorav@gmail.com>
2025-08-17 14:22:28 +0300

    chore: wrap all app with dashboard layout

--- src/components/layouts/dashboard-layout.tsx
+++ src/components/layouts/dashboard-layout.tsx
@@ -2,8 +2,8 @@ import type { ReactNode } from 'react';
 
 export default function DashboardLayout({ children }: { children: ReactNode }) {
   return (
-    <main>
-      <div></div>
+    <main className='w-full min-h-screen bg-[#060010]'>
+      <div className='text-white'>The bottom dock goes here</div>
       <div>{children}</div>
     </main>
   );
