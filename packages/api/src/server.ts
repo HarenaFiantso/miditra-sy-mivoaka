ab420b6b3a7459b9e77b19c02086232f9cbec516
Fiantso Harena <fiantsorav@gmail.com>
2025-08-17 22:44:05 +0300

    fix: remove cors issue

--- packages/api/src/server.ts
+++ packages/api/src/server.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 
 import { healthCheckRoute } from './routes';
 
 const app = express();
 const PORT = process.env.PORT ?? '8080';
 
+app.use(cors());
 app.use('/api/v1/health-check', healthCheckRoute);
 
 app.listen(PORT, () => {
