c784c0be10af4b5afaab2140d2a03e32afb4226b
Fiantso Harena <fiantsorav@gmail.com>
2025-08-17 12:41:51 +0300

    feat(ui): sign up form ui

--- src/app/routes/auth/register.tsx
+++ src/app/routes/auth/register.tsx
@@ -1,3 +1,14 @@
+import { RegisterForm } from '@/components/auth';
+import { PlasmaWave } from '@/components/shared';
+
 export default function Register() {
-  return <h1>Register</h1>;
+  return (
+    <section className="flex w-full flex-1 items-center justify-center p-6">
+      <PlasmaWave yOffset={-250} xOffset={-200} rotationDeg={-180} />
+      <PlasmaWave yOffset={250} xOffset={200} rotationDeg={30} />
+      <div className="max-w-xl overflow-hidden">
+        <RegisterForm />
+      </div>
+    </section>
+  );
 }
