55fa916da33e979509c1ba8ceda30f447c2ddf2f
Fiantso Harena <fiantsorav@gmail.com>
2025-08-18 00:00:15 +0300

    feat(api): user can login

--- packages/api/src/controllers/auth.controller.ts
+++ packages/api/src/controllers/auth.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 import { PrismaClient } from 'generated/prisma';
 
@@ -9,9 +10,48 @@ interface RegisterBody {
   password: string;
 }
 
-const prisma = new PrismaClient();
+interface LoginBody {
+  email: string;
+  password: string;
+}
 
-const login = () => {};
+const prisma = new PrismaClient();
+const JWT_SECRET = process.env.JWT_SECRET as string;
+
+const login = async (req: Request<object, object, LoginBody>, res: Response) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      res.status(400).json({ error: 'Email and password are required' });
+      return;
+    }
+
+    const user = await prisma.user.findUnique({ where: { email } });
+    if (!user) {
+      res.status(401).json({ error: 'Invalid username or password' });
+      return;
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      res.status(401).json({ error: 'Invalid username or password' });
+      return;
+    }
+
+    const token = jwt.sign({ userId: user.id, username: user.username, email: user.email }, JWT_SECRET, {
+      expiresIn: '1h',
+    });
+
+    res.status(200).json({ token: token });
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(error.message);
+      res.status(500).json({ error: `Failed to login user : ${error.message}` });
+    } else {
+      res.status(500).json({ error: 'Unknown error occurred' });
+    }
+  }
+};
 
 const register = async (req: Request<object, object, RegisterBody>, res: Response) => {
   try {
